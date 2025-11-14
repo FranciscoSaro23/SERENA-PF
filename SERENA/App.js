e4ceeeff5b7f0a0f862f4f0a8276ad19cb134b78
Android Studio <devandroid@ort>
2025-11-14 08:45:05 -0300

    punhtooooooooooo

--- SERENA/App.js
+++ SERENA/App.js
@@ -17,7 +17,7 @@ import Ayuda from './src/screens/Ayuda';
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
 
-import { BLEProvider } from '../src/context/BLEContext';
+import { BLEProvider } from './src/context/BLEContext';
 
 function MainTabs() {
   return (
